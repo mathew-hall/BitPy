e61937f41973ff2ce60cc444becf23d11d68e17e
Mathew Hall <mathew.james.hall+gh@gmail.com>
2016-02-22 00:37:31 +0000

    Move attributes to instance in torrents.py

--- BitPy/torrents.py
+++ BitPy/torrents.py
@@ -2,14 +2,10 @@ import bencode
 import hashlib
 
 class Info:
-	piece_length = None
-	pieces = None
-	private = 0
-	name = None
-	files = []
-	filemode = None
-	def __init__(self, info_struct):
 
+	def __init__(self, info_struct):
+		self.piece_length = None
+		self.files = []
 		self.private = info_struct.get('private',0)
 		self.piece_length = info_struct['piece length']
 		def chunk_values(str):
@@ -37,27 +33,26 @@ class TorrentFile:
 	(including SHA1 hashes of the pieces) and the announce value specifies the tracker(s)
 	that coordinate the swarm.
 	"""
-	info = None
-	announce = None
-	announce_list = None
-	creation_date = None
-	comment = None
-	created_by = None
-	encoding = None
+
 	
 	def __init__(self, torrent_file_contents={}):
-			for k,v in torrent_file_contents.items():
-				if k != 'info':
-					setattr(self,k.replace('-','_').replace(" ",'_'),v)
-				else:
-					self.info = Info(v)
-			
-			sha1 = hashlib.sha1()
-			sha1.update(bencode.bencode(torrent_file_contents['info']))
-			self.info_hash = sha1.digest()
-	
-	
-	
+		self.info = None
+		self.announce = None
+		self.announce_list = None
+		self.creation_date = None
+		self.comment = None
+		self.created_by = None
+		self.encoding = None
+		for k,v in torrent_file_contents.items():
+			if k != 'info':
+				setattr(self,k.replace('-','_').replace(" ",'_'),v)
+			else:
+				self.info = Info(v)
+		
+		sha1 = hashlib.sha1()
+		sha1.update(bencode.bencode(torrent_file_contents['info']))
+		self.info_hash = sha1.digest()
+		
 	def __repr__(self):
 		return repr({"info":self.info, "announce":self.announce, "announce-list":self.announce_list, "creation date": self.creation_date, "comment":self.comment, "created by": self.created_by, "encoding": self.encoding, 'info hash': self.info_hash})
 
